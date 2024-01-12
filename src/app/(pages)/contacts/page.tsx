fd89bf8e626d4d55823890f651c524c0167c8d47
GKastet <127600582+GKastet@users.noreply.github.com>
2024-01-12 15:00:59 +0100

    test

--- src/app/(pages)/contacts/page.tsx
+++ src/app/(pages)/contacts/page.tsx
@@ -1,7 +1,7 @@
 //Page Contacts
 import Link from "next/link";
 import stylesPageContacts from "./pageContacts.module.css";
-import Calculator from "@/components/Btns/BtnCalc/Calculator";
+// import Calculator from "@/components/Btns/BtnCalc/Calculator";
 
 const PageContacts = () => {
   const { titleCommon, title } = stylesPageContacts;
@@ -13,7 +13,7 @@ const PageContacts = () => {
       <h3 className={title}>Title</h3>
       <Link href='/projects'>To ProjectsPage | </Link>
       <Link href='/'>To Main</Link>
-      <Calculator/>
+      {/* <Calculator/> */}
     </>
   );
 };
