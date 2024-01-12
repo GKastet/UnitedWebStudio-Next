6b4986bf93f82a51816f4d42170ad6797f5b5f6d
GKastet <127600582+GKastet@users.noreply.github.com>
2024-01-12 15:25:45 +0100

    added metadata to pages

--- src/app/(pages)/contacts/page.tsx
+++ src/app/(pages)/contacts/page.tsx
@@ -1,7 +1,12 @@
 //Page Contacts
+import { Metadata } from "next";
 import Link from "next/link";
 import stylesPageContacts from "./pageContacts.module.css";
 import Calculator from "@/components/Btns/BtnTest/Calculator";
+
+export const metadata: Metadata = {
+  title: "Contacts",
+};
 
 const PageContacts = () => {
   const { titleCommon, title } = stylesPageContacts;
@@ -11,9 +16,9 @@ const PageContacts = () => {
       <h1>Page Contacts</h1>
       <h2 className={titleCommon}>Common Title</h2>
       <h3 className={title}>Title</h3>
-      <Link href='/projects'>To ProjectsPage | </Link>
-      <Link href='/'>To Main</Link>
-      <Calculator/>
+      <Link href="/projects">To ProjectsPage | </Link>
+      <Link href="/">To Main</Link>
+      <Calculator />
     </>
   );
 };
